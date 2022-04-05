04f1b311da859b3f7a4922b346ac83074fb7a09a
hossay <youhs4554@gmail.com>
2022-04-05 10:43:26 +0900

    Rename package name 'disjrnet'->'disjrnet_pytorch'

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.md', encoding='utf-8') as f: # README.md 내용 읽어오기
     long_description = f.read()
 
 setup(
-    name                = 'disjrnet',
+    name                = 'disjrnet_pytorch',
     version             = '0.1',
     long_description    = long_description,          # readme
     long_description_content_type = 'text/markdown', # markdown
